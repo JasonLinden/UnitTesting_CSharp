194fac5192c2ed28390260779c89f586bce77a42
Jason <jason.linden@zapper.com>
2018-12-13 16:53:45 +0200

    Initial commit

---
+++ TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            // Arrange
+            var math = new Fundamentals.Math();
+
+            // Act
+            var result = math.Add(1, 2);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        // Try and add all the scenarios implementing the tests
+
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnFirstArgument()
+        {
+
+        }
+
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnSecondArgument()
+        {
+
+        }
+
+        [Test]
+        public void Max_ArgumentAreEqual_ReturnSameArgument()
+        {
+
+        }
+    }
+}
---
+++ TestNinja/TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+
+    // Add TestMethods must be public void
+
+    [TestFixture]
+    public class ReservationTests
+    {
+        // CanBeCanceledBy_Scenario_ExpectedBehaviour
+        [Test]
+        public void CanBeCanceledBy_UserIsAdmin_ReturnsTrue()
+        {
+            // Arrange - Prepare
+            var reservation = new Reservation();
+
+            // Act - call the method to test.
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            // Assert
+            Assert.IsTrue(result);
+            // Assert.That(result, Is.True);
+            // Assert.That(result == true);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_UserMadeReservation_ReturnsTrue()
+        {
+            // Arrange 
+            var user = new User() { IsAdmin = false };
+            var reservation = new Reservation() { MadeBy = user };
+            
+            // Act
+            var result = reservation.CanBeCancelledBy(user);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void CanBeCanceledBy_UserNotAdminOrMade_ReturnsFalse()
+        {
+            // Arrange
+            var user = new User { IsAdmin = false };
+            var reservation = new Reservation();
+
+            // Act
+            var result = reservation.CanBeCancelledBy(user);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
